81355ecc9a0590ad6a65afa53806f5fb4024c3f2
anudeep22003 <anudeep.yegireddi@gmail.com>
2016-11-30 14:15:41 -0500

    All gene work

--- frontpage/models.py
+++ frontpage/models.py
@@ -5,15 +5,22 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-class Answer(models.Model):
+class Question(models.Model):
 
-	key = models.CharField(max_length=200)
 	author = models.ForeignKey('auth.User')
 	question = models.TextField()
-	content = models.TextField()
-	published_date = models.DateTimeField(blank=True, null=True)
-	upvotes = models.IntegerField()
-	views = models.IntegerField()
+	question_date = models.DateTimeField(blank=True, null=True)
 
 	def __str__(self):
-		return self.key
+		return self.question
+
+class Answer(models.Model):
+
+	author = models.ForeignKey('auth.User')
+	question = models.ForeignKey(Question)
+	content = models.TextField()
+	answer_date = models.DateTimeField(blank=True, null=True)
+	
+	def __str__(self):
+		return self.question
+
