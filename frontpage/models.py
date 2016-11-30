4b73d86b769750eaa64885432a78c970d95eaf13
Siddharth Jain <siddjain2607@gmail.com>
2016-11-30 12:15:01 -0500

    Added models for answer
    Added models for answer

--- frontpage/models.py
+++ frontpage/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils import timezone
 # Create your models here.
+
+class Answer(models.Model):
+
+	key = models.CharField(max_length=200)
+	author = models.ForeignKey('auth.User')
+	question = models.TextField()
+	content = models.TextField()
+	published_date = models.DateTimeField(blank=True, null=True)
+	upvotes = models.IntegerField()
+	views = models.IntegerField()
+
+	def __str__(self):
+		return self.key
